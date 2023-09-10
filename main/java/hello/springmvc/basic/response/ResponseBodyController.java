fb0d2e4c9085349814600aca8e1b881d412c2981
junhui-LEE <junhee0386@naver.com>
2023-09-10 16:55:23 +0900

    HTTP 응답 - HTTP API, 메시지 바디에 직접 입력 김영한 MVC 1편의 부분 강의중 쉬운 파트이다. 바로 커밋 앤 푸시 한다.

---
+++ main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,89 @@
+package hello.springmvc.basic.response;
+
+import hello.springmvc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+//@Controller
+@RestController
+public class ResponseBodyController {
+
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException{
+        response.getWriter().write("ok");
+    } // -> 이것이 가장 단순한 방법이다.
+
+//    HttpEntity, ResponseEntity (Http Status 추가)
+//    @Return
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() throws IOException{
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    } // -> 이렇게 하는 것도 있다.
+
+    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() throws IOException{
+        return "ok";
+    }
+
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1(){
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2(){
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userB");
+        helloData.setAge(30);
+        return helloData;
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
