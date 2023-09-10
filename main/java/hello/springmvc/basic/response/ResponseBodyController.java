ef7470eaed0461e43e8b9f91f297d118fb20a458
junhui-LEE <junhee0386@naver.com>
2023-09-10 23:33:26 +0900

    1) HTTP 메시지 컨버터 2) 요청 매핑 헨들러 어댑터 구조 이것은 김영한 mvc 1편 부분강의 2개의 분량이다. 사실 입력하는 코드가 없어서 커밋을 올릴것이 없는데 내가 학습을 했다는 것을 로그로 남기고 싶어서 띄어쓰기만 있는 커밋을 올린다. 이 두개의 강의는 spring 아키텍처에서 핸들러 어댑터가 컨트롤러를 호출할때 ArgumentResolver을 호출하고 컨트롤러의 인자로 들어가는 값을 반환하는 ArgumentResolver가 @ModelAttribute나 사용자 객체(메서드의 사용자 객체 클래스, 데이터타입)을 보고 jacksonMessageConverter나 ModelAttribute에 넣는 객체를 핸들러 어댑터에 제공하고 핸들러 어댑터가 컨트롤러(핸들러)에 받은 인자를 넣으면서 호출하고 컨트롤러가 ReturnValue를 호출하고 등등을 다루는 이론이다.

--- main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
